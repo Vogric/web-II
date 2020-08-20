ba27a0be772f1a71cefc026cf2b983f46407290a
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-08-20 13:38:58 -0300

    tp1

---
+++ Unidad-1-Arquitecturas-web/Practico-Unidad-1/ej-2.php
@@ -0,0 +1,10 @@
+<h1>Ejercicio 3</h1>
+
+<?php
+  echo('<ul>');
+  for ($i=0; $i < 10 ; $i++){ 
+    $cadena = ("<li>$i</li>");
+    echo($cadena);
+  }
+  echo('</ul>');
+?>
---
+++ Unidad-1-Arquitecturas-web/Practico-Unidad-1/ej-3.php
@@ -0,0 +1,8 @@
+<?php
+  $nombre = $_POST['nombre']; 
+  $apellido = $_POST['apellido'];
+  $edad = $_POST['edad'];
+  echo('Nombre:'.$nombre);
+  echo('Apellido:'.$apellido);
+  echo('Edad:'.$edad);
+?>
