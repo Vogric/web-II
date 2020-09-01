5035bfbdf16430d532a9d483c83b94e835624005
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-09-01 09:42:58 -0300

    Correccion Ej-9

--- Unidad-1-Arquitecturas-web/Practico-Unidad-1/ej-9.php
+++ Unidad-1-Arquitecturas-web/Practico-Unidad-1/ej-9.php
@@ -1,18 +1,22 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$edad = $_POST['edad'];
-$genero = $_POST['genero'];
-$pais = $_POST['pais'];
-foreach ( $_POST['intereses'] as $interes );
-
 if ( !empty( $_POST['nombre'] ) && !empty( $_POST['apellido'] ) && !empty( $_POST['edad'] )
     && isset( $_POST['genero'] ) &&
     isset( $_POST['pais'] ) ) {
 
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $edad = $_POST['edad'];
+    $genero = $_POST['genero'];
+    $pais = $_POST['pais'];
+    $intereses = isset( $_POST['intereses'] ) ? $_POST['intereses'] : array();
+
     echo ( "Nombre: $nombre <br> Apellido: $apellido <br> Edad: $edad <br>
-    Genero: $genero <br> País: $pais <br> Interes elegido: $interes" );
+    Genero: $genero <br> País: $pais <br>" );
+
+    foreach ( $intereses as $interes ) {
+        echo ( "$interes <br>" );
+    }
 
 } else {
     echo ( "Todos los campos deben estar completos" );
